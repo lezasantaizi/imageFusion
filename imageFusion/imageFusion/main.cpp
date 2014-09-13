41ef9c232f41fee1ff6b437d0e141712c98a34de
renxiaoming <xiaomingx.ren@intel.com>
2014-09-13 18:03:50 +0800

    initial the project

---
+++ imageFusion/imageFusion/main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include "../3rdparty/include/opencv.hpp"
+#include<math.h>
+using namespace std;
+using namespace cv;
+
+using namespace std;
+
+#pragma comment(lib, "opencv_highgui249d.lib")
+#pragma comment(lib, "opencv_imgproc249d.lib")
+#pragma comment(lib, "opencv_core249d.lib")
+#pragma comment(lib, "opencv_highgui249.lib")
+#pragma comment(lib, "opencv_imgproc249.lib")
+#pragma comment(lib, "opencv_core249.lib")
+
+int main(int argc, char** argv)
+{
+	return 0;
+}
